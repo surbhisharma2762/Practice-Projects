38a0d01af288d2279a626f3f615f0558a20fe6f8
surbhi sharma <surbhisharma1581@gmail.com>
2023-12-16 23:44:06 +0530

    javascript programs

---
+++ javascript/app.js
@@ -0,0 +1,131 @@
+// let light="red";
+// if(light=="red"){
+//     console.log(`Stop`);
+// }
+// if(light=="yellow"){
+//     console.log(`Slow Down`);
+// }
+// if(light=="green"){
+//     console.log(`Go`);
+// }
+
+// let marks=42;
+// if(marks>80){
+//     console.log('A+');
+// }
+// else if(marks>=60){
+//     console.log('A');
+// }
+// else if(marks>=33){
+//     console.log('B');
+// }
+//  else if(marks<33){
+//     console.log('C');
+// }
+
+// let size="XL";
+// if(size==="S"){
+//     console.log('price=50');
+// }
+// else if(size==="M"){
+//     console.log('price=100');
+// }
+// else if(size==="L"){
+//     console.log('price=200');
+// }
+// else if(size==="XL"){
+//     console.log('price=250');
+// }
+
+// let Str="jirl";
+// if (Str.length>3 && Str[0]==='a'){
+//     console.log('good string');
+// }
+// else{
+//     console.log('bad string');
+// }
+
+// let day="7";
+// switch(day){
+//     case "1":
+//         console.log('Today is Monday');
+//         break;
+//     case "2":
+//         console.log("Tuesday");
+//         break;
+//     case "3":
+//         console.log("Wed");
+//         break;
+//     case "4":
+//         console.log("thu");
+//         break;
+//      case "5":
+//         console.log("Fri");
+//         break;
+//     case "6":
+//         console.log("Sat");
+//         break;
+//     case "7":
+//         console.log("Sund");
+//         break;    
+//     default:
+//         console.log("Invalid day");
+// }
+
+// let arr=[0,7,3,-2];
+// let n=3;
+// let ans=arr.slice(0,n);
+// console.log(ans);
+
+// let arr=[1,5,6,9];
+// let num=3;
+// let ans=arr.slice(arr.length-num);
+// console.log(ans);
+
+//a string is blank or not
+// let str=prompt("Enter a string");
+// if (str.length==0){
+//     console.log("This string is empty");
+// }
+// else{
+//     console.log("str is not empty");
+// }
+
+//character at the given index is Lower Case.
+// let str="ApNA Collehe";
+// let ind=3;
+// if (str[ind]==str[ind].toLowerCase()){
+//     console.log("str in lowercase");
+// }
+// else{
+//     console.log("str is not in lowercase");
+// }
+
+//removing whitespaces form strat and end(strip and trailing spaces)
+// let str=prompt("Enter a str");
+// console.log(`original string is : ${str}`);
+// console.timeLog(`str without whitespace ${str.trim()}`);
+
+//check element presenst in array
+// let arr=[4,8,10,12,15,18,20];
+// let num=4;
+// if(arr.indexOf(num) != -1){
+//     console.log("element present");
+// }
+// else{
+//     console.log("Element not present");
+// }
+
+// let num=10;
+// let div=10;
+// if (num/div){
+//     console.log("divisible");
+// }
+// else{
+//     console.log("not divisible");
+// }
+    
+//multiplication of 5
+for(i=5;i<=50;i=i+5){
+    console.log(i);
+}
